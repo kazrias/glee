3901dc05e581e53ef80da9784e5dbcf5ff29036d
kazrias <ka6rias@gmail.com>
2022-10-01 04:50:32 +0400

    fixed js, added top-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
-$(function(){
-  
+$(function () {
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
+  })
 });
